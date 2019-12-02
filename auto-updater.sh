db8adba1fbe243da65bf892f7ddba3e67ed91e4d
koen84 <koen84@users.noreply.github.com>
2019-12-02 23:54:47 +0100

    auto-updater.sh scripts live in $CUSTOM_HOME
    While often $CUSTOM_HOME = $HOME, should be mindful to use the correct variable in case it's not.

--- auto-updater.sh
+++ auto-updater.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+source config/variables.cfg
+
 #Handle some paths
 export GOPATH=$HOME/go
 export PATH=$GOPATH/bin:$PATH
@@ -28,7 +30,7 @@ if [ -z "$LATEST" ]; then
               if [ "$CLONEDTAG" != "$LATEST" ]; then
                             echo "Latest tag on github: $LATEST" >> $HOME/autoupdate.status
                             echo "Triggering automated upgrade !" >> $HOME/autoupdate.status
-                            cd $HOME/elrond-go-scripts-v2/ && bash script.sh auto_upgrade
+                            cd $CUSTOM_HOME/elrond-go-scripts-v2/ && bash script.sh auto_upgrade
                               
                               else
                                 echo "Latest tag on github: $LATEST" >> $HOME/autoupdate.status
